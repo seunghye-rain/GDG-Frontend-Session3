6402a73d79a9ceba260827f557248b0c033dcb93
bhyerain <seunghye0307@naver.com>
2024-10-05 12:34:34 +0900

    [Feat] 초기 설정

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,59 @@
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import logo from "./logo.png";
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className="flex flex-col justify-center items-center bg-gradient-to-t from-black via-red-950 to-black w-full h-full font-custom">
+      {/* 상단 로고와 로그인 버튼 */}
+      <div className="flex flex-row justify-between items-center w-full p-8">
+        <div className="flex">
+          <Image src={logo} alt="Netflix Logo" className="w-[148px] h-[50px]" />
+        </div>
+        <div className="flex">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            role="button"
+            className="w-auto h-auto p-2 bg-red-600 text-sm text-white font-custom rounded-sm"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            로그인
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      {/* 메인 콘텐츠 */}
+      <div className="p-32 text-center">
+        <div>
+          <h1 className="text-5xl font-extrabold leading-snug text-white">
+            영화, 시리즈 등을 <br />
+            무제한으로
+          </h1>
+          <p className="text-lg font-normal text-white mt-3 mb-6">
+            5,500원으로 시작하세요. 멤버십은 언제든지 해지 가능합니다.
+          </p>
+        </div>
+
+        {/* 이메일 입력 필드와 시작하기 버튼 */}
+        <div className="flex flex-col items-center">
+          <h3 className="text-md font-normal text-white mt-3 mb-6">
+            시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일
+            주소를 입력하세요.
+          </h3>
+          <div className="flex flex-row w-[508px] h-[50px] justify-center p-1">
+            {/* 이메일 입력 필드 */}
+            <input
+              type="email"
+              placeholder="이메일 주소"
+              className="w-full p-4 bg-gray-600 bg-opacity-20 border-2 border-white text-sm text-white font-thin rounded-l-md focus:outline-none"
+            />
+            {/* 시작하기 버튼 */}
+            <a
+              role="button"
+              className="p-4 bg-red-600 text-lg text-white font-medium rounded-r-md border-red-600"
+            >
+              시작하기
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
